dca6da54c8ba8eaf8ab2b5ed12b3f87dd6ae99fd
Lioncash <mathew1800@gmail.com>
2021-06-22 09:24:15 -0400

    assembler: Implement FCVT.S.W

--- tests/src/assembler_rvf_tests.cpp
+++ tests/src/assembler_rvf_tests.cpp
@@ -35,6 +35,24 @@ TEST_CASE("FCLASS.S", "[rv32f]") {
     REQUIRE(value == 0xE00F93D3);
 }
 
+TEST_CASE("FCVT.S.W", "[rv32f]") {
+    uint32_t value = 0;
+    Assembler as(reinterpret_cast<uint8_t*>(&value), sizeof(value));
+
+    as.FCVT_S_W(f31, x7, Assembler::RMode::RNE);
+    REQUIRE(value == 0xD0038FD3);
+
+    as.RewindBuffer();
+
+    as.FCVT_S_W(f31, x7, Assembler::RMode::RMM);
+    REQUIRE(value == 0xD003CFD3);
+
+    as.RewindBuffer();
+
+    as.FCVT_S_W(f31, x7, Assembler::RMode::DYN);
+    REQUIRE(value == 0xD003FFD3);
+}
+
 TEST_CASE("FCVT.W.S", "[rv32f]") {
     uint32_t value = 0;
     Assembler as(reinterpret_cast<uint8_t*>(&value), sizeof(value));
