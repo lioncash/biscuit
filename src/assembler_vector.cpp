2dc8a266eca03c04b29875d14588e78309f187fc
Lioncash <mathew1800@gmail.com>
2021-07-19 06:59:20 -0400

    assembler_vector: Implement VREDMAXU

--- src/assembler_vector.cpp
+++ src/assembler_vector.cpp
@@ -485,6 +485,10 @@ void Assembler::VREDAND(Vec vd, Vec vs2, Vec vs1, VecMask mask) noexcept {
     EmitVectorOPMVV(m_buffer, 0b000001, mask, vs2, vs1, vd);
 }
 
+void Assembler::VREDMAXU(Vec vd, Vec vs2, Vec vs1, VecMask mask) noexcept {
+    EmitVectorOPMVV(m_buffer, 0b000110, mask, vs2, vs1, vd);
+}
+
 void Assembler::VREDMIN(Vec vd, Vec vs2, Vec vs1, VecMask mask) noexcept {
     EmitVectorOPMVV(m_buffer, 0b000101, mask, vs2, vs1, vd);
 }
