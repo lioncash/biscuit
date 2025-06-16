4dc46bd0c0895a51581860ac81b8170b25c4e687
Lioncache <mai.iam2048@gmail.com>
2025-06-16 03:24:38 -0400

    assert: Allow specifying custom assert handlers
    Allows for more flexibility when it comes to handling errors on various
    platform types. Though the default suffices for most uses, it might be
    more beneficial for some to pipe the messages through some other means
    that might be more convenient, etc.

--- include/biscuit/assert.hpp
+++ include/biscuit/assert.hpp
@@ -3,6 +3,23 @@
 #include <cstdio>
 #include <cstdlib>
 
+// The asserting mechanism for biscuit can be overridden
+//
+// The default assertion mechanism is simply a printf of the failing
+// condition followed by an abort. However depending on one's particular
+// setup, this might be unnecessary, odd, lacking, or some other
+// undesirable quality.
+//
+// This allows the user to specify their own assertion handler to be used
+// in place of the default. The user can even opt to define an empty handler
+// should the checking prove to slow down performance (and you're reeeeally
+// sure you won't make any mistakes with the API, or making a release build).
+//
+// However keep in mind that defining an empty handler means you're also
+// signing up to debug your own missteps, should they happen, that would've
+// likely been caught otherwise.
+#ifndef BISCUIT_ASSERT
+
 #define BISCUIT_ASSERT(condition)                                        \
   do {                                                                   \
     if (!(condition)) {                                                  \
@@ -12,3 +29,5 @@
       std::abort();                                                      \
     }                                                                    \
   } while (false)
+
+#endif
